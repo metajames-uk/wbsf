6a04d39b2890c61f85b9999751eb4ed08b7d363f
James Hollands <james.hollands@pushdr.com>
2019-12-02 19:59:20 +0000

    got to start of video 5

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Home = (props) => {
+    return <div>
+        <p>Hello</p>
+        
+    </div>
+}
+
+export default Home
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Sell = (props) => {
+    return <div>
+        <p>Sell page</p>
+        
+    </div>
+}
+
+export default Sell
